a85d371e571c6e7fa4b09daf9df0ed223c235d45
Kolchin13D <kolchin13d@gmail.com>
2024-07-23 14:24:39 +0300

    add functions

--- app/src/main/java/com/example/ocr/MainActivity.java
+++ app/src/main/java/com/example/ocr/MainActivity.java
@@ -1,28 +1,41 @@
 package com.example.ocr;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 1;
     ImageView imageView;
     TextView textView;
     Button selectBtn, recogBtn;
@@ -73,7 +86,60 @@ public class MainActivity extends AppCompatActivity {
 
     private void OpenGallety() {
 
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/");
 
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/");
 
+        Intent chooseIntent = Intent.createChooser(getIntent, "select Image");
+        chooseIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+        startActivityForResult(chooseIntent, PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE){
+            if (data != null){
+                byte[] bytes = new byte[0];
+                String filePath = null;
+
+                try {
+                    inputImage = InputImage.fromFilePath(this, data.getData());
+                    Bitmap resultUri = inputImage.getBitmapInternal();
+                    Glide.with(MainActivity.this)
+                            .load(resultUri)
+                            .into(imageView);
+
+                    // process the image
+                    Task<Text> result = recognizer.process(inputImage)
+                            .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text text) {
+                                    RecogniseTextBlock(text);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.v("ERROR1", e.getMessage());
+                                    Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                } catch (IOException e) {
+                    Log.v("ERROR2", e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
+    private void RecogniseTextBlock(Text text) {
     }
 }
